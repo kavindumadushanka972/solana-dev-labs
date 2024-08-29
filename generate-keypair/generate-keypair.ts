14b02c1e43649c4da4ee8a96fecbaacbb60b46bc
Kavindu Madushanka <kavindumadushanka972@gmail.com>
2024-08-29 22:48:45 +0530

    first commit

---
+++ generate-keypair/generate-keypair.ts
@@ -0,0 +1,17 @@
+// TODO: run `npx esrun generate-keypair.ts` to test the script
+
+import { Keypair } from '@solana/web3.js';
+import 'dotenv/config';
+import { getKeypairFromEnvironment } from '@solana-developers/helpers';
+
+const keypair = Keypair.generate();
+console.log(`The public key is: `, keypair.publicKey.toBase58());
+console.log(`The secret key is: `, keypair.secretKey);
+console.log(`✅ Finished!`);
+
+const keypairFromEnv = getKeypairFromEnvironment('SECRET_KEY');
+console.log(`The public key is: `, keypairFromEnv.publicKey.toBase58());
+console.log(`The secret key is: `, keypairFromEnv.secretKey);
+console.log(
+  `✅ Finished! We've loaded our secret key securely, using an env file!`
+);
