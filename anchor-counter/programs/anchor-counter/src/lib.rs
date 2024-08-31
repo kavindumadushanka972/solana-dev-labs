2c41786b44e4043216c7c0cda4f99d0600568ece
Kavindu Madushanka <kavindumadushanka972@gmail.com>
2024-08-31 10:05:01 +0530

    anchor counter

---
+++ anchor-counter/programs/anchor-counter/src/lib.rs
@@ -0,0 +1,45 @@
+use anchor_lang::prelude::*;
+
+declare_id!("7hDkaK6M8dQstrG7GERbuej9V7ak9FF4LatzcFjvW8po");
+
+#[program]
+pub mod anchor_counter {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        counter.count = 0;
+        msg!("Counter Account Created");
+        msg!("Current Count: { }", counter.count);
+        Ok(())
+    }
+
+    pub fn increment(ctx: Context<Update>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        msg!("Previous counter: {}", counter.count);
+        counter.count = counter.count.checked_add(1).unwrap();
+        msg!("Counter incremented. Current count: {}", counter.count);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Update<'info> {
+    #[account(mut)]
+    pub counter: Account<'info, Counter>,
+    pub user: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 8 + 8)]
+    pub counter: Account<'info, Counter>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct Counter {
+    pub count: u64,
+}
