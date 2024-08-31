c4a3269d18ab5530f351ece16f253eb7342ac303
Kavindu Madushanka <kavindumadushanka972@gmail.com>
2024-08-31 10:14:13 +0530

    Update: rust code

--- anchor-counter/programs/anchor-counter/src/lib.rs
+++ anchor-counter/programs/anchor-counter/src/lib.rs
@@ -9,8 +9,7 @@ pub mod anchor_counter {
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         let counter = &mut ctx.accounts.counter;
         counter.count = 0;
-        msg!("Counter Account Created");
-        msg!("Current Count: { }", counter.count);
+        msg!("Counter account created. Current count: {}", counter.count);
         Ok(())
     }
 
@@ -24,19 +23,19 @@ pub mod anchor_counter {
 }
 
 #[derive(Accounts)]
-pub struct Update<'info> {
-    #[account(mut)]
-    pub counter: Account<'info, Counter>,
-    pub user: Signer<'info>,
-}
-
-#[derive(Accounts)]
 pub struct Initialize<'info> {
     #[account(init, payer = user, space = 8 + 8)]
     pub counter: Account<'info, Counter>,
     #[account(mut)]
     pub user: Signer<'info>,
     pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Update<'info> {
+    #[account(mut)]
+    pub counter: Account<'info, Counter>,
+    pub user: Signer<'info>,
 }
 
 #[account]
